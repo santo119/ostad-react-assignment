15c0f04b1d4006043173fccdc806b9304cce6c11
santo119 <srswebsanto49@gmail.com>
2025-03-04 05:01:07 +0600

    added sidebar and header section

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,62 @@
 
 
 import './App.css'
+import { LuTableOfContents } from "react-icons/lu";
+import MyCard from './components/MyCard';
+import SideBar from './layouts/SideBar';
+import { useEffect, useState } from 'react';
+import Header from './layouts/Header';
 
 function App() {
 
+  const [sidebarOpen, setSidebarOpen] = useState(false)
+  useEffect(() => {
+  console.log(sidebarOpen)
+  },[sidebarOpen])
+
   return (
     <>
-      
-      <h1>Vite + React</h1>
+      {/* first section  */}
+
+    <div className='flex h-screen bg-[#E6EFF5] w-screen md:overflow-x-hidden' >
+        {/* sidebar */}
+
+        
+        <SideBar
+        sidebarOpen={sidebarOpen}
+        setSidebarOpen={setSidebarOpen}
+        ></SideBar>
+       
+        {/* content */}
+
+        <div className='flex-1 flex flex-col'>
+          {/* header */}
+         
+          <Header
+          sidebarOpen={sidebarOpen}
+          setSidebarOpen={setSidebarOpen}
+          >
+
+          </Header>
+
+          {/* main content */}
+
+          <main className='flex-1 p-4 overflow-auto  container mx-auto mt-1'>
+            
+            {/* summaryCard, summaryTemp, recent Transaction  */}
+            
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-5 overflow-x-auto md:overflow-visible scroll-smooth snap-x snap-mandatory">
+            <MyCard></MyCard>
+           </div>
+
+          </main>
+
+         {/* footer */}
+
+        </div>
+
+    </div>
+     
       
     </>
   )
